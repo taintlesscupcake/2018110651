2b3661271f49b9c028babde23302fc0643af5f8e
신동해 <rlfwkd3@khu.ac.kr>
2021-10-08 10:40:07 +0900

    Add gugudan.cpp

---
+++ gugudan.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+	for (int i = 1; i <= 8; i++) {
+		for (int j = 1; j <= 8; j++) {
+			cout << i << " * " << j << " = " << i * j << endl;
+		}
+		cout << endl;
+	}
+
+	return 0;
+}
